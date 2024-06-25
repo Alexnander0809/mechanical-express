992f799ca2551c1e54a549e8da7239aebd5b021c
Alexander_08 <alexvalderramaguisao@gmail.com>
2024-06-25 01:13:23 -0500

    Si se pued

--- mechanical_express/admin.py
+++ mechanical_express/admin.py
@@ -1,22 +1,23 @@
 from django.contrib import admin
 from .models import Usuario, Mecanico
+from django.core.mail import send_mail
 
-admin.site.register(Usuario),
-admin.site.register(Mecanico)
+admin.site.register(Usuario)
 
-class TuModeloAdmin(admin.ModelAdmin):
+class MecanicoAdmin(admin.ModelAdmin):
     def delete_model(self, request, obj):
-        # Aquí puedes personalizar la lógica para enviar el correo electrónico de notificación
-        subject = 'Objeto eliminado: {}'.format(obj.__class__.__name__)
-        message = 'Se ha eliminado el objeto {} con ID {}'.format(obj.__class__.__name__, obj.id)
-        from_email = 'tu@email.com'
-        to_emails = ['destinatario1@email.com', 'destinatario2@email.com']
+        # Lógica para enviar el correo electrónico de notificación
+        subject = 'Cuenta eliminada en Mechanical Express'
+        message = f"Estimado usuario, su cuenta en Mechanical Express ha sido eliminada debido a acumulación de denuncias. Por favor, póngase en contacto con el soporte si tiene alguna pregunta."
+
+        from_email = 'soportemechanicalexpress@email.com'
+        to_emails = Mecanico.objects.values_list('email', flat=True)
         
-        # Envía el correo electrónico de notificación
+        # Envío del correo electrónico de notificación
         send_mail(subject, message, from_email, to_emails)
         
         # Llama al método delete_model predeterminado para eliminar el objeto
-        super(TuModeloAdmin, self).delete_model(request, obj)
+        super().delete_model(request, obj)
 
 # Registra tu modelo personalizado con el administrador de Django
-admin.site.register(TuModelo, TuModeloAdmin)
+admin.site.register(Mecanico, MecanicoAdmin)
